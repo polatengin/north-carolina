435e4c51c413a028efb867cae15e8c0464a32a29
polatengin <enpolat@microsoft.com>
2024-06-13 06:31:32 -0700

    appending csharp object to a blob as a json line

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Text.Json;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+
+var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING") ?? "";
+var containerName = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONTAINER_NAME") ?? "";
+var blobName = Environment.GetEnvironmentVariable("AZURE_STORAGE_BLOB_NAME") ?? "";
+
+var blobServiceClient = new BlobServiceClient(connectionString);
+var blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+await blobContainerClient.CreateIfNotExistsAsync();
+
+var appendBlobClient = blobContainerClient.GetAppendBlobClient(blobName);
+
+await appendBlobClient.CreateIfNotExistsAsync();
+
+var line = new { name = $"John Doe {Random.Shared.NextInt64()}", age = Random.Shared.Next(18, 99), timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()};
+
+using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(line) + Environment.NewLine)))
+{
+  await appendBlobClient.AppendBlockAsync(stream);
+}
